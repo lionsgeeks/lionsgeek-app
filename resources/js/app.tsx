96efbc292a29dd13dc1dd3074e942df1c9f8b6f4
youssef-labib <youssef.labib2023@gmail.com>
2025-08-27 12:08:31 +0100

    revert: remove loading component integration due to bugs

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,45 +1,12 @@
 import '../css/app.css';
 
-import React, { useState, useEffect } from 'react';
-import { createInertiaApp, router } from '@inertiajs/react';
+import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 // import { initializeTheme } from './hooks/use-appearance';
 import { AppContextProvider } from "./context/appContext";
-import LoadingPage from './components/loadingPage.jsx';
 
 const appName = 'LionsGeek';
-
-// App wrapper component to handle loading state
-function AppWrapper({ App, props }: { App: React.ComponentType<any>; props: any }) {
-    const [isLoading, setIsLoading] = useState(false);
-
-    useEffect(() => {
-        // Listen to Inertia navigation events
-        const handleStart = () => setIsLoading(true);
-        const handleFinish = () => setIsLoading(false);
-
-        const removeStartListener = router.on('start', handleStart);
-        const removeFinishListener = router.on('finish', handleFinish);
-        const removeErrorListener = router.on('error', handleFinish);
-
-        return () => {
-            removeStartListener();
-            removeFinishListener();
-            removeErrorListener();
-        };
-    }, []);
-
-    if (isLoading) {
-        return <LoadingPage />;
-    }
-
-    return (
-        <AppContextProvider>
-            <App {...props} />
-        </AppContextProvider>
-    );
-}
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -47,7 +14,11 @@ createInertiaApp({
     setup({ el, App, props }) {
         const root = createRoot(el);
 
-        root.render(<AppWrapper App={App} props={props} />);
+        root.render(
+            <AppContextProvider>
+                <App {...props} />
+            </AppContextProvider>
+        );
     },
     progress: {
         color: '#fee819',
