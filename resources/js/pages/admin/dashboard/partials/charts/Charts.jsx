e0f2cc1af5c34281d650c250cbfc4b26f66ed1d9
yahya mws <yahyamoussair05@gmail.com>
2025-09-10 13:13:32 +0100

    feat: add select promo form make sessio filter by promo

--- resources/js/pages/admin/dashboard/partials/charts/Charts.jsx
+++ resources/js/pages/admin/dashboard/partials/charts/Charts.jsx
@@ -11,13 +11,13 @@ import { DonutChart } from './components/PieChart.js';
 const Chart = () => {
     const { allsessions } = usePage().props;
 
-    // remove private sessions
     const sessions = allsessions.filter((session) => session.name.toLowerCase() !== 'private session');
-
-    // default session is the last one
     const defaultSessionID = sessions[sessions.length - 1]?.id || null;
 
     const [selectedSession, setSelectedSession] = useState(defaultSessionID);
+    const [AllPromo, setAllPromo] = useState([]);
+    const [AllSessions, setAllSessions] = useState([]);
+    const [selectedPromo, setSelectedPromo] = useState(AllPromo[0]);
     const [barChart, setBarChart] = useState([]);
     const [pieChart, setPieChart] = useState([]);
 
@@ -33,38 +33,71 @@ const Chart = () => {
             .catch((err) => console.error('Error fetching chart data:', err));
     }, [selectedSession]);
 
+    useEffect(() => {
+        const allPromo = [
+            ...new Set(
+                allsessions
+                    .filter((e) => e.name.includes(':'))
+                    .map((promo) => promo.name.slice(0, promo.name.indexOf(':')).trim())
+            ),
+        ];
+        setAllPromo(allPromo);
+    }, [allsessions]);
+
+    useEffect(() => {
+        if (AllPromo.length > 0) {
+            setSelectedPromo(AllPromo[0]);
+        }
+    }, [AllPromo]);
+
+    useEffect(() => {
+        if (!selectedPromo) return;
+
+        const newtab = allsessions.filter((session) => session.name.includes(selectedPromo));
+        setAllSessions(newtab);
+    }, [selectedPromo, allsessions]);
+
     return (
         <Card className="w-full">
-            {/* Header with title + select */}
             <CardHeader className="flex lg:flex-row flex-col lg:gap-5 gap-10 lg:items-center justify-between">
-                {/* <div className="flex items-center gap-2">
-                    <BarChart2 color="#ffc803" className="h-6 w-6" />
-                    </div> */}
                 <div className="flex items-center gap-3">
                     <div className="rounded-lg bg-gray-100 p-2">
                         <BarChart2 className="h-5 w-5 text-[#212529]" />
                     </div>
-                    {/* <h2 className="text-lg font-semibold text-[#212529]">Recent Newsletters</h2> */}
                     <CardTitle className="text-xl font-semibold">Analyse</CardTitle>
                 </div>
 
-                <Select value={selectedSession} onValueChange={(value) => setSelectedSession(value)}>
-                    <SelectTrigger className="lg:w-[20%] w-full">
-                        <SelectValue placeholder="Select Session" />
-                    </SelectTrigger>
-                    <SelectContent>
-                        {sessions.map((session) => (
-                            <SelectItem key={session.id} value={session.id}>
-                                {session.name}
-                            </SelectItem>
-                        ))}
-                    </SelectContent>
-                </Select>
+                <div className="flex flex-col lg:w-[30%] w-full gap-5">
+                    <Select value={selectedPromo} onValueChange={(value) => setSelectedPromo(value)}>
+                        <SelectTrigger className="w-full">
+                            <SelectValue placeholder="Select Promo" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            {AllPromo?.map((promo, index) => (
+                                <SelectItem key={index} value={promo}>
+                                    {promo}
+                                </SelectItem>
+                            ))}
+                        </SelectContent>
+                    </Select>
+
+                    <Select value={selectedSession} onValueChange={(value) => setSelectedSession(value)}>
+                        <SelectTrigger className="w-full">
+                            <SelectValue placeholder="Select Session" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            {AllSessions?.map((session) => (
+                                <SelectItem key={session.id} value={session.id}>
+                                    {session.name}
+                                </SelectItem>
+                            ))}
+                        </SelectContent>
+                    </Select>
+                </div>
             </CardHeader>
 
-            {/* Content with charts */}
             <CardContent>
-                <div className="flex lg:flex-row-reverse gap-5 flex-col ">
+                <div className="flex lg:flex-row-reverse gap-5 flex-col">
                     <DonutChart pieChart={pieChart} id={selectedSession} />
                     <BarChart barChart={barChart} />
                 </div>
