4d14c8e4ab67829bdc1c6cdda0eea141e80ee588
elroumaissaa <aakamelrou@gmail.com>
2025-09-10 16:36:06 +0200

    feat(auth,dashboard): track and display last login in admin users table

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Mail\AddAdminMail;
+use App\Mail\ResetPasswordMail;
 use Illuminate\Support\Facades\Mail;
 use App\Models\Mode;
 use App\Models\User;
@@ -35,6 +36,30 @@ class UserController extends Controller
         Mail::to($request->email)->send(new AddAdminMail($randomPassword));
 
 
+        return back();
+    }
+
+    public function resetPassword(Request $request)
+    {
+        $request->validate([
+            'user_id' => ['required', 'exists:users,id'],
+        ]);
+
+        $user = User::findOrFail($request->input('user_id'));
+        $newPassword = Str::random(8);
+        $user->password = Hash::make($newPassword);
+        $user->save();
+
+        // Send with dedicated reset email template
+        Mail::to($user->email)->send(new ResetPasswordMail($newPassword));
+
+        return back();
+    }
+
+    public function destroy(User $user)
+    {
+        // Prevent deleting yourself optionally could be handled here
+        $user->delete();
         return back();
     }
 }
