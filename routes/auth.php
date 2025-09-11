3bac6cc926fd3b564ece9f2181a5b35b9da35f2b
aymen-abbad-andaloussi <aymenandaloussi69@gmail.com>
2025-09-11 13:10:41 +0100

    refactor : change url login to geek-in && forgot-password to geek-reset

--- routes/auth.php
+++ routes/auth.php
@@ -16,15 +16,15 @@ Route::middleware('guest')->group(function () {
 
     // Route::post('register', [RegisteredUserController::class, 'store']);
 
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
+    Route::get('geek-in', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
 
-    Route::post('login', [AuthenticatedSessionController::class, 'store']);
+    Route::post('geek-in', [AuthenticatedSessionController::class, 'store']);
 
-    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
+    Route::get('geek-reset', [PasswordResetLinkController::class, 'create'])
         ->name('password.request');
 
-    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
+    Route::post('geek-reset', [PasswordResetLinkController::class, 'store'])
         ->name('password.email');
 
     Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -12,7 +12,7 @@ class DashboardTest extends TestCase
 
     public function test_guests_are_redirected_to_the_login_page()
     {
-        $this->get('/admin/dashboard')->assertRedirect('/login');
+        $this->get('/admin/dashboard')->assertRedirect('/geek-in');
     }
 
     public function test_authenticated_users_can_visit_the_dashboard()
