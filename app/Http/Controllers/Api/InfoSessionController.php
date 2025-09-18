4454a0c177321cf1ce3b7d065ae1f64eb5e6cbc2
Ayman Boujjar <aymenboujjar12@gmail.com>
2025-09-18 11:32:35 +0100

    fix media session problem

--- app/Http/Controllers/Api/InfoSessionController.php
+++ app/Http/Controllers/Api/InfoSessionController.php
@@ -94,8 +94,8 @@ class InfoSessionController extends Controller
 
     public function infoData(Request $request)
     {
-        // $session = InfoSession::find($request->id);
-        $session = InfoSession::first();
+        $session = InfoSession::find($request->id);
+        // $session = InfoSession::first();
 
         $participant = $session->participants()->get();
         $attended = $session->participants()->where("is_visited", 1)->orderby("updated_at", "desc")->get();
